83b5bee75e34f35d2c60728e974e45c62bb9fdee
betelnegusu22 <betelnegusu22@gmail.com>
2026-02-02 06:02:39 +0300

    Use Cloudinary URLs for all videos

---
+++ src/components/Videos.jsx
@@ -0,0 +1,11 @@
+const videos = {
+  vid1: "https://res.cloudinary.com/dz67dmf9k/video/upload/v1769987699/vid27_oqyfej.mp4",
+  vid2: "https://res.cloudinary.com/dz67dmf9k/video/upload/v1769987438/vid2_nyp2um.mp4",
+  vid3: "https://res.cloudinary.com/dz67dmf9k/video/upload/v1769987693/vid26_uwbddv.mp4",
+   vid3: "https://res.cloudinary.com/dz67dmf9k/video/upload/v1769987693/vid26_uwbddv.mp4",
+  // ...
+  // ...
+  // continue until vid34
+};
+
+export default videos;
